92cc0c7686b0d02dd96ac1a8ea4073459aa77bbb
Saurav Singh Mohanta <sauravsinghmohanta44@gmail.com>
2026-06-10 00:20:51 +0530

    Add files via upload

---
+++ EdgeBrowserTest.java
@@ -0,0 +1,109 @@
+package com.BrowserTest.Examples;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class EdgeBrowserTest {
+
+	
+	@Test
+	public void EdgeTest1() {
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String URL = driver.getCurrentUrl();
+		System.out.println(URL);
+	}
+	
+	@Test
+	public void EdgeTest2() {
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://demo.opencart.com/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String URL = driver.getCurrentUrl();
+		System.out.println(URL);
+	}
+	
+	@Test
+	public void EdgeTest3() {
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.bing.com");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String URL = driver.getCurrentUrl();
+		System.out.println(URL);
+	}
+	
+	@Test
+	public void EdgeTest4() {
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.freecrm.com/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String URL = driver.getCurrentUrl();
+		System.out.println(URL);
+	}
+	
+	@Test
+	public void EdgeTest5() {
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.chase.com/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String URL = driver.getCurrentUrl();
+		System.out.println(URL);
+	}
+	
+	@Test
+	public void EdgeTest6() {
+		WebDriver driver = new EdgeDriver();
+		driver.get("http://altoro.testfire.net/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String URL = driver.getCurrentUrl();
+		System.out.println(URL);
+	}
+	
+	@Test
+	public void EdgeTest7() {
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String URL = driver.getCurrentUrl();
+		System.out.println(URL);
+	}
+	
+	@Test
+	public void EdgeTest8() {
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String URL = driver.getCurrentUrl();
+		System.out.println(URL);
+	}
+	
+	@Test
+	public void EdgeTest9() {
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.demoblaze.com/index.html");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String URL = driver.getCurrentUrl();
+		System.out.println(URL);
+	}
+	
+}
